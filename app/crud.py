698028df101a1a65650e3c11d08129c3f5e7fbdf
xenon54v <kokona955@gmail.com>
2026-05-31 02:09:57 +0300

    apd crud
    people can not take others tasks

--- app/crud.py
+++ app/crud.py
@@ -1,18 +1,51 @@
 from sqlalchemy.orm import Session
-from app.models import Task
-from app.schemas import TaskCreate, TaskUpdate
 
-def get_tasks(db: Session):
-    return db.query(Task).all()
+from app.models import Task, User
+from app.schemas import TaskCreate, TaskUpdate, UserCreate
+from app.security import get_password_hash, verify_password
 
-def get_task_by_id(db: Session, task_id: int):
-    return db.query(Task).filter(Task.id == task_id).first()
+def get_user_by_username(db: Session, username: str):
+    return db.query(User).filter(User.username == username).first()
 
-def create_task(db: Session, task_data: TaskCreate):
+def create_user(db: Session, user_data: UserCreate):
+    new_user = User(
+        username=user_data.username,
+        hashed_password=get_password_hash(user_data.password)
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+def authenticate_user(db: Session, username: str, password: str):
+    user = get_user_by_username(db, username)
+
+    if user is None:
+        return None
+
+    if not verify_password(password, user.hashed_password):
+        return None
+
+    return user
+
+def get_tasks(db: Session, user_id: int):
+    return db.query(Task).filter(Task.owner_id == user_id).all()
+
+def get_task_by_id(db: Session, task_id: int, user_id: int):
+    return (
+        db.query(Task)
+        .filter(Task.id == task_id, Task.owner_id == user_id)
+        .first()
+    )
+
+def create_task(db: Session, task_data: TaskCreate, user_id: int):
     new_task = Task(
         title=task_data.title,
         description=task_data.description,
-        is_completed=task_data.is_completed
+        is_completed=task_data.is_completed,
+        owner_id=user_id
     )
 
     db.add(new_task)
@@ -21,8 +54,13 @@ def create_task(db: Session, task_data: TaskCreate):
 
     return new_task
 
-def update_task(db: Session, task_id: int, task_data: TaskUpdate):
-    task = get_task_by_id(db, task_id)
+def update_task(
+    db: Session,
+    task_id: int,
+    task_data: TaskUpdate,
+    user_id: int
+):
+    task = get_task_by_id(db, task_id, user_id)
 
     if task is None:
         return None
@@ -37,8 +75,8 @@ def update_task(db: Session, task_id: int, task_data: TaskUpdate):
 
     return task
 
-def delete_task(db: Session, task_id: int):
-    task = get_task_by_id(db, task_id)
+def delete_task(db: Session, task_id: int, user_id: int):
+    task = get_task_by_id(db, task_id, user_id)
 
     if task is None:
         return None
